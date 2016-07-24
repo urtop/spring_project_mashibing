696efda2fde4c7f3e18238edca4ea78fa9d400bc
Mark Tao <t22w@qq.com>
2016-07-24 19:46:47 +0800

    version 2 phase1

---
+++ src/com/bjsxt/registeration/model/User.java
@@ -0,0 +1,34 @@
+package com.bjsxt.registeration.model;
+
+/**
+ * Created by Mark on 2016/7/24.
+ */
+public class User {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String username;
+    private String password;
+    private int id;
+}
---
+++ src/com/bjsxt/registeration/service/UserManager.java
@@ -0,0 +1,7 @@
+package com.bjsxt.registeration.service;
+
+/**
+ * Created by Mark on 2016/7/24.
+ */
+public class UserManager {
+}
