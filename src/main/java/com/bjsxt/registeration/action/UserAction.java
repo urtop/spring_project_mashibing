11ec2956de75e2a94804dbe66342925a38ae6661
Mark Tao <t22w@qq.com>
2016-07-27 22:33:42 +0800

    尼玛的，修复了struts2 报404的错误问题，原来是user.action页面返回的字符串和struts.xml里的success字符串不匹配，去你妈的,SB java。。。。

--- src/main/java/com/bjsxt/registeration/action/UserAction.java
+++ src/main/java/com/bjsxt/registeration/action/UserAction.java
@@ -6,8 +6,11 @@ import com.opensymphony.xwork2.ActionSupport;
  * Created by  Mark L Tao on 2016/7/27 17:58.
  */
 public class UserAction extends ActionSupport {
+    private String username;
+    private String password;
+
     @Override
     public String execute() throws Exception {
-        return "Success";
+        return "success";
     }
 }
