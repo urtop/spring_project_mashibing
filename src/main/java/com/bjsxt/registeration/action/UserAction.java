29179c4d6f6fa1f5759f7f0f0870452aadd0aff1
Mark L Tao <mark.l.tao@noexternalmail.hsbc.com>
2016-07-28 19:15:57 +0800

    Add struts2 support phase 1

--- src/main/java/com/bjsxt/registeration/action/UserAction.java
+++ src/main/java/com/bjsxt/registeration/action/UserAction.java
@@ -1,5 +1,8 @@
 package com.bjsxt.registeration.action;
 
+import com.bjsxt.registeration.model.User;
+import com.bjsxt.registeration.service.UserManager;
+import com.bjsxt.registeration.service.impl.UserManagerImpl;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -9,8 +12,41 @@ public class UserAction extends ActionSupport {
     private String username;
     private String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserManager getUserManager() {
+        return userManager;
+    }
+
+    public void setUserManager(UserManager userManager) {
+        this.userManager = userManager;
+    }
+
+    private UserManager userManager = new UserManagerImpl();
+
     @Override
     public String execute() throws Exception {
+        User user =new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        if(userManager.exists(user)){
+            return "fail";
+        }
+        userManager.add(user);
         return "success";
     }
 }
